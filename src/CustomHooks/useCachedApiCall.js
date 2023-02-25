e26dba40ca61b098fc2ee640d5effae60501cf3c
abdulsamad245 <100769627+abdulsamad245@users.noreply.github.com>
2023-02-25 21:45:55 +0100

    Update useCachedApiCall.js

--- src/CustomHooks/useCachedApiCall.js
+++ src/CustomHooks/useCachedApiCall.js
@@ -20,9 +20,11 @@ export default function useCachedApiCall(API_KEY, path_prefix = "") {
       setErrorMsg("");
       setResponse(null);
       axios
-        .get(
-          `${path_prefix}${api_path}&api_key=${API_KEY}&with_genres=${_genre}&primary_release_date.gte=${releaseYearStart}&primary_release_date.lte=${releaseYearEnd}&with_runtime.gte=90`
-        )
+//         .get(
+//           `${path_prefix}${api_path}&api_key=${API_KEY}&with_genres=${_genre}&primary_release_date.gte=${releaseYearStart}&primary_release_date.lte=${releaseYearEnd}&with_runtime.gte=90`
+//         )
+       .get(
+          'https://api.themoviedb.org/3/discover/movie?api_key=12345&with_genres=28&primary_release_date.gte=2022-01-01&primary_release_date.lte=2022-12-31' )
 
         .then((res) => {
           cache.current[api_path] = res.data;
