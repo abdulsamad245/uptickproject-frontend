9b335a766ae4ba207155aa16a0d182d6e54d4789
abdulsamad245 <100769627+abdulsamad245@users.noreply.github.com>
2023-03-01 21:57:50 +0100

    Update useMovieSearch.js

--- src/CustomHooks/useMovieSearch.js
+++ src/CustomHooks/useMovieSearch.js
@@ -1,28 +1,46 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from "react";
 import useCachedApiCall from "./useCachedApiCall";
 
+export default function () {
+  const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
 
-export default function(){
-    const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
+  const [mediaType, setMediaType] = useState("movie");
+  const [searchQuery, setSearchQuery] = useState("");
+  const [_genre, setGenre] = useState("");
+  const [YearStart, setYearStart] = useState("");
+  const [YearEnd, setYearEnd] = useState("");
+  const [isInitial, setInitial] = useState(true);
+  const [page, setPage] = useState(1);
+  const searchAPI = useCachedApiCall(API_KEY, "https://api.themoviedb.org/3");
 
-    const [mediaType, setMediaType] = useState("movie");
-    const [searchQuery, setSearchQuery] = useState("");
-    const [page, setPage] = useState(1);
-    const searchAPI = useCachedApiCall(API_KEY, "https://api.themoviedb.org/3");
-    
-    const performAPICall = () => {
-        if(searchQuery){
-            const api_query = `/search/${mediaType}?query=${searchQuery}&page=${page}&include_adult=false&language=en-US`;
-            console.log("[Use Movie Search] ", api_query);
-            searchAPI.apiCall(api_query);
-        }
+  const performAPICall = () => {
+    if (searchQuery) {
+      const api_query = `/search/${mediaType}?api_key=${API_KEY}&query=${searchQuery}&page=${page}&include_adult=false&language=en-US`;
+      console.log("[Use Movie Search] ", api_query);
+      searchAPI.apiCall(api_query, true);
     }
+  };
 
-    useEffect(()=>{
-        performAPICall();        
-    },[searchQuery, page, mediaType]);
+  useEffect(() => {
+    performAPICall();
+  }, [searchQuery, page, mediaType, YearStart, YearEnd, _genre]);
 
-
-    return {mediaType, setMediaType, searchQuery, setSearchQuery, page, setPage, performAPICall, searchAPI};
-
+  return {
+    mediaType,
+    setMediaType,
+    searchQuery,
+    setSearchQuery,
+    _genre,
+    setGenre,
+    YearStart,
+    setYearStart,
+    YearEnd,
+    setYearEnd,
+    page,
+    setPage,
+    performAPICall,
+    searchAPI,
+    isInitial,
+    setInitial,
+  };
 }
