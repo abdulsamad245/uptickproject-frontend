a85c0dac34de8f2e69cf1575d23523570d729890
abdulsamad245 <100769627+abdulsamad245@users.noreply.github.com>
2023-03-01 21:58:29 +0100

    Update useCachedApiCall.js

--- src/CustomHooks/useCachedApiCall.js
+++ src/CustomHooks/useCachedApiCall.js
@@ -3,51 +3,61 @@ import axios from "axios";
 
 export default function useCachedApiCall(API_KEY, path_prefix = "") {
   const cache = useRef({});
+  var api_req;
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
   const [errorMsg, setErrorMsg] = useState("");
   const [response, setResponse] = useState(null);
+  const [_genre, setGenre] = useState("");
+  const [YearStart, setYearStart] = useState("");
+  const [YearEnd, setYearEnd] = useState("");
 
-  const apiCall = (
-    api_path,
-    _genre = "",
-    releaseYearStart = "",
-    releaseYearEnd = ""
-  ) => {
+  const apiCall = (api_path, isSearch = false) => {
     if (api_path) {
-//       setLoading(true);
-//       setError(false);
-//       setErrorMsg("");
-//       setResponse(null);
-      console.log(api_path,path_prefix);
-      const _url = `${path_prefix}${api_path}&api_key=${API_KEY}&with_genres=${_genre}&primary_release_date.gte=${releaseYearStart}&primary_release_date.lte=${releaseYearEnd}&with_runtime.gte=90`
-      console.log(  `${path_prefix}${api_path}&api_key=${API_KEY}&with_genres=${_genre}&primary_release_date.gte=${releaseYearStart}&primary_release_date.lte=${releaseYearEnd}&with_runtime.gte=90`)
-      axios
-//         .get(
-//           `https://api.themoviedb.org/3${api_path}&api_key=${API_KEY}&with_genres=${_genre}&primary_release_date.gte=${releaseYearStart}&primary_release_date.lte=${releaseYearEnd}&with_runtime.gte=90`
-//         )
-              .get(
-          _url
-        )
-//        .get(
-//          'https://api.themoviedb.org/3/discover/movie?api_key=2b7be3a2f5b8c75236be6a4e648ca9b2&with_genres=28&primary_release_date.gte=2022-01-01&primary_release_date.lte=2022-12-31' )
-
-        .then((res) => {
-          cache.current[api_path] = res.data;
-          setResponse(res.data);
-          console.log(res.data);
-        })
-        .catch((err) => {
-          setError(true);
-          console.log("[API ERROR]", err.message, err);
-          setErrorMsg(err.message);
-        })
-        .then(() => setLoading(false));
+      console.log("API req:", api_path);
+      if (cache.current[api_path]) {
+        console.log("Getting data from cache...", cache.current[api_path]);
+        setError(false);
+        setErrorMsg("");
+        setResponse(cache.current[api_path]);
+      } else {
+        if (isSearch) {
+          api_req = `${path_prefix}${api_path}`;
+        } else {
+          api_req = `${path_prefix}${api_path}&api_key=${API_KEY}&with_genres=${_genre}&primary_release_date.gte=${YearStart}&primary_release_date.lte=${YearEnd}&with_runtime.gte=90`;
+        }
+        axios
+          .get(api_req)
+          .then((res) => {
+            cache.current[api_path] = res.data;
+            setResponse(res.data);
+            console.log(res.data);
+          })
+          .catch((err) => {
+            setError(true);
+            console.log("[API ERROR]", err.message, err);
+            setErrorMsg(err.message);
+          })
+          .then(() => setLoading(false));
+      }
     } else {
       setError(true);
       setErrorMsg("API Request was empty!");
     }
+    return;
   };
 
-  return { loading, error, errorMsg, response, apiCall };
+  return {
+    loading,
+    error,
+    errorMsg,
+    response,
+    apiCall,
+    _genre,
+    YearStart,
+    YearEnd,
+    setGenre,
+    setYearStart,
+    setYearEnd,
+  };
 }
